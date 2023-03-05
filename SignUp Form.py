363496ba2b318b7885b568fac77c7ace51bd7d31
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2023-03-05 11:30:51 +0500

    Add files via upload

---
+++ SignUp Form.py
@@ -0,0 +1,47 @@
+import sqlite3 as s3
+import pandas as pd
+print('Please Fill The Given Form')
+
+name = str(input('Enter Your name : '))
+
+age = int(input('Enter YOur age : '))
+
+phone = int(str(input('Enter your Phone Number : ')))
+
+#create_table ='''create table studennt (
+ #                   id int auto incremented,
+  #                  sname varchar,
+   #                 age int ,  
+    #                phone int )'''
+
+
+con = s3.connect('student.db' , timeout=10)
+cur = con.cursor()
+data = [(1,name , age , phone)]
+
+sql = '''insert into studennt values (?,?,?,?)'''
+
+#res = cur.execute(create_table) 
+
+res1 = cur.executemany(sql , data)
+
+
+res3 = '''select * from studennt'''
+
+res4 = cur.execute(res3)
+
+res_final = cur.fetchall()
+
+res = pd.DataFrame([res_final])
+
+print(res)
+
+
+con.commit()
+
+con.close()
+
+
+
+
+
